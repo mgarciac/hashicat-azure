5d929e4dac753271be34042b91ef59f57a1d71db
root <michael.garcia.88@gmail.com>
2020-07-22 22:15:54 +0000

    teat

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,17 @@
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+}
+
 module "network" {
-  source  = "app.terraform.io/jmgc001-training/network/azurerm"
-  version = "3.1.1"
-  resource_group_name = "jmgc001-rg"
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
